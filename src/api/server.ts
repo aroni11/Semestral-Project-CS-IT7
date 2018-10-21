a3f0c8f5602eb10d76ccebaf731dfdc1cbd9852b
Josef Kriz <email@josefkriz.com>
2018-10-21 20:45:37 +0200

    use async instead of callback

--- src/api/server.ts
+++ src/api/server.ts
@@ -12,8 +12,7 @@ app.listen(SERVER_PORT, () => {
   console.log(`Server is up and listening on port ${SERVER_PORT}`);
 });
 
-app.get('/api/list/:collection', (req, res) => {
-  const maxResults: number = req.query.limit === undefined ? 50000 : Number(req.query.limit);
+app.get('/api/list/:collection', async (req, res) => {
   let collection;
 
   switch (req.params.collection) {
@@ -28,12 +27,16 @@ app.get('/api/list/:collection', (req, res) => {
       break;
     default:
       res.status(400).send('Invalid collection');
+      return;
   }
 
-  collection.find({'tags.highway': {$in: GAR_ROADS}}, null, { limit: maxResults }, (err, list) => {
-    if (err) { res.status(503).send(err); }
-    res.send(list);
-  });
+  const maxResults: number = req.query.limit === undefined ? 50000 : Number(req.query.limit);
+
+  try {
+    res.send(await collection.find({}, null, { limit: maxResults }));
+  } catch (e) {
+    res.status(503).send(e);
+  }
 });
 
 app.get('/api/:collection/:id', async (req, res) => {
