b9bfef525bff933681f4dc41dbe3c4dcefba72ef
aroni11 <bigron86@gmail.com>
2018-10-23 12:01:24 +0200

    Commented the methods in graph.ts

--- src/models/graph.ts
+++ src/models/graph.ts
@@ -1,25 +1,43 @@
 import Edge from './edge'
 import Vertex from './vertex';
 
+/**
+ * Graph object for creating a sample graph using Vertex and Edge objects
+ */
+
 class Graph {
-    private readonly points: Map<number, Vertex>;        
+    /**
+     * Map objects for the graph sample
+     */    
+    private readonly vertices: Map<number, Vertex>;        
     private readonly edges: Map<number, Edge>;
 
+    /**
+     * Initializes new Map objects for the Vertices and Edges displayed on the graph
+     */
     constructor() {
-        this.points = new Map<number, Vertex>();
+        this.vertices = new Map<number, Vertex>();
         this.edges = new Map<number, Edge>();
     }
 
+    /**
+     * Adds a Vertex object into the vertices Map object
+     * @param x : Vertex object
+     */
     addVertex(x: Vertex) : void {
-        this.points.set(x.id, x);
+        this.vertices.set(x.id, x);
     }
 
+    /**
+     * Adds a Edge object into the edges Map object
+     * @param x : Edge object 
+     */
     addEdge(x: Edge): void {
         this.edges.set(x.id, x);
     }
 
     get graph() {
-        return { vertices: this.points,
+        return { vertices: this.vertices,
                  edges: this.edges };
     }
 }
