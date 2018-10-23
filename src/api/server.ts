3dba8e246b6aedd3f965bb9ec88f973829430236
Josef Kriz <email@josefkriz.com>
2018-10-23 16:22:00 +0200

    add endpoint for retrieving nodes within a polygon

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,11 +1,15 @@
+import bodyParser from 'body-parser';
 import express from 'express';
 import mongoose, {ConnectionOptions} from 'mongoose';
 import {MONGO_OPTIONS, MONGO_URI, SERVER_PORT} from '../../config';
 import {findByIdHandler} from './handlers/findById';
 import {listHandler} from './handlers/list';
+import {nodesWithinHandler} from './handlers/nodesWithin';
 import {roadsHandler} from './handlers/roads';
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(SERVER_PORT, () => {
   connect(MONGO_URI, MONGO_OPTIONS);
@@ -17,6 +21,8 @@ app.get('/api/list/:collection', listHandler);
 app.get('/api/:collection/:id', findByIdHandler);
 
 app.get('/api/roads', roadsHandler);
+
+app.post('/api/within', nodesWithinHandler);
 
 function connect(mongoURI: string, options: ConnectionOptions): void {
   mongoose.set('useCreateIndex', true);
