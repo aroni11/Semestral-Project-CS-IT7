ebe17debae7ce3d18fe8ccd66841963354914bb1
aroni11 <bigron86@gmail.com>
2018-10-24 14:44:06 +0200

    Changes to graph object for future sample creation.

--- src/models/graph.ts
+++ src/models/graph.ts
@@ -1,43 +1,40 @@
-import Edge from './edge'
 import Vertex from './vertex';
 
 /**
  * Graph object for creating a sample graph using Vertex and Edge objects
  */
 
-class Graph {
+export default class Graph {
     /**
      * Map objects for the graph sample
      */    
     private readonly vertices: Map<number, Vertex>;        
-    private readonly edges: Map<number, Edge>;
 
     /**
      * Initializes new Map objects for the Vertices and Edges displayed on the graph
      */
     constructor() {
         this.vertices = new Map<number, Vertex>();
-        this.edges = new Map<number, Edge>();
     }
 
     /**
-     * Adds a Vertex object into the vertices Map object
+     * Adds a Vertex object or number of Vertex objects into the vertices Map object
      * @param x : Vertex object
      */
-    addVertex(x: Vertex) : void {
-        this.vertices.set(x.id, x);
+    addVertex(...x: Vertex[]) : void {
+        for(const v of x) {
+            this.vertices.set(v.id, v);
+        }
     }
 
     /**
-     * Adds a Edge object into the edges Map object
-     * @param x : Edge object 
+     * Get function which returns a certain Vertex inside the Vertices dictionary
+     * @param id : The ID of the Vertex object to be returned
      */
-    addEdge(x: Edge): void {
-        this.edges.set(x.id, x);
-    }
-
-    get graph() {
-        return { vertices: this.vertices,
-                 edges: this.edges };
+    getVertex(id : number) {
+        if(!(id in this.vertices))
+            throw new Error('Vertex ID not found!')
+        else
+            return this.vertices.get(id);           
     }
 }
