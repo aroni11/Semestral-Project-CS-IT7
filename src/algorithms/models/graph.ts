850bf7e249d2dcccc1c2b028d0b994a415435b17
Vlad Saioc <vs@obiplus.com>
2018-11-03 15:35:52 +0100

    * Removed error throwing if Vertex ID is not found in graph

--- src/algorithms/models/graph.ts
+++ src/algorithms/models/graph.ts
@@ -5,43 +5,40 @@ import Vertex from './vertex';
  */
 
 export default class Graph {
-    /**
-     * Map objects for the graph sample
-     */
-    private readonly _vertices: Map<number, Vertex>;
+  /**
+   * Map objects for the graph sample
+   */
+  private readonly _vertices: Map<number, Vertex>;
 
-    /**
-     * Initializes new Map objects for the Vertices displayed on the graph
-     */
-    constructor() {
-        this._vertices = new Map<number, Vertex>();
-    }
+  /**
+   * Initializes new Map objects for the Vertices displayed on the graph
+   */
+  constructor() {
+    this._vertices = new Map<number, Vertex>();
+  }
 
-    /**
-     * Adds a Vertex object or number of Vertex objects into the vertices Map object
-     * @param x : Vertex object
-     */
-    addVertex(...x: Vertex[]): void {
-        for (const v of x) {
-            this._vertices.set(v.id, v);
-        }
+  /**
+   * Adds a Vertex object or number of Vertex objects into the vertices Map object
+   * @param x : Vertex object
+   */
+  addVertex(...x: Vertex[]): void {
+    for (const v of x) {
+      this._vertices.set(v.id, v);
     }
+  }
 
-    /**
-     * Get function which returns a certain Vertex inside the Vertices dictionary
-     * @param id : The ID of the Vertex object to be returned
-     */
-    getVertex(id: number): Vertex {
-        if (!this._vertices.has(id)) {
-            throw new Error('Vertex ID not found!');
-        }
-        return this._vertices.get(id);
-    }
+  /**
+   * Get function which returns a certain Vertex inside the Vertices dictionary
+   * @param id : The ID of the Vertex object to be returned
+   */
+  getVertex(id: number): Vertex {
+    return this._vertices.get(id);
+  }
 
-    /*
-    * Get all vertices in this graph
-    */
-    get vertices(): Iterator<Vertex> {
-      return this._vertices.values();
-    }
+  /*
+  * Get all vertices in this graph
+  */
+  get vertices(): Iterator<Vertex> {
+    return this._vertices.values();
+  }
 }
