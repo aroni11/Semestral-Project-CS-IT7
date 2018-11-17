8d6641b02bfdc783562fd4eab63051c562d646f2
Vlad Saioc <vs@obiplus.com>
2018-11-17 12:26:47 +0100

    * Roundabout consideration for one way

--- src/algorithms/helpers/one-way.ts
+++ src/algorithms/helpers/one-way.ts
@@ -2,9 +2,12 @@ import { IWay } from '../../api/schema/way';
 
 export default (way: IWay): boolean => {
   if (way.tags) {
-    const keys = Object.keys(way.tags);
-    const index = keys.findIndex((x) => x === 'oneway');
-    return index !== -1;
+    const tags: any = way.tags;
+    const conditions = [
+      tags.oneway === true,
+      tags.junction === 'roundabout'
+    ];
+    return conditions.reduce((prev, next) => prev || next);
   }
   return false;
 };
