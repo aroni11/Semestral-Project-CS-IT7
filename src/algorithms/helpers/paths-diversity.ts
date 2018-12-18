af785365a88518f6a71d5973ed9a0de8f1072eac
Honza Kopecký <honza.kopecky95@gmail.com>
2018-12-18 14:04:47 +0100

    Fix tslint errors

--- src/algorithms/helpers/paths-diversity.ts
+++ src/algorithms/helpers/paths-diversity.ts
@@ -2,7 +2,7 @@ import Path from '../pathfinders/path';
 
 function getDiversity(shortest: Path, current: Path): number {
   const missingEdges = new Path();
-  for(const edge of current.pathData) {
+  for (const edge of current.pathData) {
     if (shortest.pathData.indexOf(edge) === -1) {
       missingEdges.pathData.push(edge);
     }
@@ -15,20 +15,22 @@ function getDiversity(shortest: Path, current: Path): number {
   return diversity > 1 ? 1 : diversity;
  }
 
- export function printDiversities(paths: Path[]): void {
-    let maxDiversity = -1;
-    let minDiversity = 2;
-    let sumDiversity = 0;
-    for (const path of paths.slice(1)) {
-      const diversity = getDiversity(paths[0], path);
-      if (diversity > maxDiversity)
-        maxDiversity = diversity;
-      if (diversity < minDiversity)
-        minDiversity = diversity;
-      sumDiversity = sumDiversity + diversity;
+export function printDiversities(paths: Path[]): void {
+  let maxDiversity = -1;
+  let minDiversity = 2;
+  let sumDiversity = 0;
+  for (const path of paths.slice(1)) {
+    const diversity = getDiversity(paths[0], path);
+    if (diversity > maxDiversity) {
+      maxDiversity = diversity;
     }
-    console.log('Diversities: ');
-    console.log('Min: ' + minDiversity.toString());
-    console.log('Max: ' + maxDiversity.toString());
-    console.log('Avg: ' + (sumDiversity / (paths.length - 1)).toString());
- }
+    if (diversity < minDiversity) {
+      minDiversity = diversity;
+    }
+    sumDiversity = sumDiversity + diversity;
+  }
+  console.log('Diversities: ');
+  console.log('Min: ' + minDiversity.toString());
+  console.log('Max: ' + maxDiversity.toString());
+  console.log('Avg: ' + (sumDiversity / (paths.length - 1)).toString());
+}
